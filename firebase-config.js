aac164024ebdbb4e0077658c30482c162c2f2437
Nrlsb <Nrlsb@users.noreply.github.com>
2025-06-22 12:28:29 +0000

    deploy: ba6bebfdce9133b5a18f05f86c20b021b5f9c91e

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: AIzaSyBogsJTYqw8x8pV6DSVMTdROF-88hGZDGk,
+  authDomain: calculadora-comisiones-29213.firebaseapp.com,
+  projectId: calculadora-comisiones-29213,
+  storageBucket: calculadora-comisiones-29213.firebasestorage.app,
+  messagingSenderId: 989161312785,
+  appId: 1:989161312785:web:1096db83de55a53eb797b0,
+  measurementId: G-FVKKH6YB0W
+};
